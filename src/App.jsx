8d46b6f8608b9cc3934bc2cf7cce66e5ed47d9fe
aladdinalizada <tu7c5zneb@code.edu.az>
2023-09-11 23:49:05 +0400

    complete LoginPage

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,39 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import CreatePost from "./pages/CreatePost";
+import { useState } from "react";
+import { signOut } from "firebase/auth";
+import { auth } from "./firebase-config";
+
 function App() {
+  const [isAuth, setIsAuth] = useState(localStorage.getItem("isAuth"));
+
+  const signUserOut = () => {
+    signOut(auth).then(() => {
+      localStorage.clear();
+      setIsAuth(false);
+      window.location.pathname = "/login";
+      window.location.reload();
+    });
+  };
+
   return (
     <Router>
       <nav>
         <Link to="/">Home</Link>
-        <Link to="/create-post">Create Post</Link>
-        <Link to="/login">Login</Link>
+        {!isAuth ? (
+          <Link to="/login"> Login </Link>
+        ) : (
+          <>
+            <Link to="/createpost"> Create Post </Link>
+            <button onClick={signUserOut}> Log Out</button>
+          </>
+        )}
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
         <Route path="/create-post" element={<CreatePost />} />
+        <Route path="/login" element={<Login setIsAuth={setIsAuth} />} />
       </Routes>
     </Router>
   );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,24 @@
+import { auth, provider } from "../firebase-config";
+import { signInWithPopup } from "firebase/auth";
+
 const Login = () => {
-  return <div>Login</div>;
+  const signInWithGoogle = ({ setIsAuth }) => {
+    signInWithPopup(auth, provider).then(() => {
+      localStorage.setItem("isAuth", true);
+      setIsAuth(true);
+      // redirect to home page
+      window.location.pathname = "/";
+      window.location.reload();
+    });
+  };
+  return (
+    <div className="loginPage">
+      <p>Sign in With Google to Continue</p>
+      <button className="login-with-google-btn" onClick={signInWithGoogle}>
+        Sign in with Google
+      </button>
+    </div>
+  );
 };
 
 export default Login;
